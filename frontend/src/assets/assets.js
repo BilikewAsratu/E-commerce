04932bfbfe33ccc5ec36b21b1e5a36521c9e26e9
BilikewAsratu <bilikew80@gmail.com>
2026-08-05 11:19:21 +0300

    add some procuts to favorite section

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -190,7 +190,7 @@ export const products = [
         subCategory: "Topwear",
         sizes: ["S", "M", "L", "XL"],
         date: 1716622345448,
-        bestseller: false
+        bestseller: true
     },
     {
         _id: "aaaai",
@@ -202,7 +202,7 @@ export const products = [
         subCategory: "Topwear",
         sizes: ["M", "L", "XL"],
         date: 1716621235448,
-        bestseller: false
+        bestseller: true
     },
     {
         _id: "aaaaj",
