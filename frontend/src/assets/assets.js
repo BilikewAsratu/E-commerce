fd84b0510e3d54e20dc84c668eefa73d2b609349
BilikewAsratu <bilikew80@gmail.com>
2026-08-21 15:29:10 +0300

    adjusting data value prepared for search

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -134,7 +134,7 @@ export const products = [
     },
     {
         _id: "aaaad",
-        name: "Men Round Neck Pure Cotton T-shirt",
+        name: "Bilikew Pure Cotton T-shirt",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 110,
         image: [p_img4],
