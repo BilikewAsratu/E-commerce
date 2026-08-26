76f9cfbfcc0670dc4ae99d893f489e613fda6993
BilikewAsratu <bilikew80@gmail.com>
2026-08-26 14:19:34 +0300

    finished user login and registration controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -9,7 +9,25 @@ const createToken = (id) => {
 
 //route for user login
 const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: "user does not exist" })
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            const token = createToken(user._id)
+            res.json({ success: true, token })
+        }
+        else {
+            res.json({ success: false, message: "invalid credentials" })
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
 
+    }
 }
 
 //route for user register
